4e626f03dfe368c30d22030d5155d1315b7d713d
Matt-Nicholas <fostermatt82@gmail.com>
2016-08-11 20:58:10 -0700

    Add js functions to toggle suggestions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){ //Waits until the page is loaded to run JS
+  var suggetion;
   $("form#survey").submit(function(event){ //Runs when th esubmit button is pressed
     event.preventDefault();
     var interests = $("#interests").val();
@@ -8,22 +9,49 @@ $(document).ready(function(){ //Waits until the page is loaded to run JS
     var cpny = $("input:radio[name=cpny]:checked").val();
 
     if(interests == "0" && (whichEnd == "back" || func == "work")){
-      alert("C#/.NET");
+      localStorage.suggestion = "cSharp";
     }
     else if(interests == "1" && (whichEnd == "back" || func == "work")){
       alert("JAVA/Android");
+      localStorage.suggestion = "java";
     }
     else if(interests == "2" && (whichEnd == "back" || func == "work")){
       alert("PHP/Drupal");
+      localStorage.suggestion = "php";
     }
     else if(interests == "3" && (whichEnd == "back" || func == "work")){
       alert("RUBY/Rails");
+      localStorage.suggestion = "ruby";
     }
     else if(interests == "4" && (whichEnd == "front" || func == "pretty")){
       alert("CSS/Design");
+      localStorage.suggestion = "css";
     }
     else{
-      alert("na")
+      alert("none");
+      localStorage.suggestion = "none";
+    }
+
+
+  });
+  $(".reveal").click(function(){
+    if(localStorage.suggestion === "cSharp"){
+      $(".cSharp").fadeToggle();
+    }
+    else if(localStorage.suggestion === "java"){
+      $(".java").fadeToggle();
+    }
+    else if(localStorage.suggestion === "php"){
+      $(".php").fadeToggle();
+    }
+    else if(localStorage.suggestion === "ruby"){
+      $(".ruby").fadeToggle();
+    }
+    else if(localStorage.suggestion === "css"){
+      $(".css").fadeToggle();
+    }
+    else if(localStorage.suggestion === "none"){
+      $(".none").fadeToggle();
     }
   });
 });
